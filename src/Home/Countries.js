c1045745d8219daa05f9a3ddd6ace4c1e12c6df2
Dan Ngetich <dancanmilgo73@gmail.com>
2021-08-06 17:17:11 +0300

    fixed hanling input

--- src/Home/Countries.js
+++ src/Home/Countries.js
@@ -3,13 +3,15 @@ import Country from "./Country";
 
 export default function Countries({ countries, query, region, theme }) {
   if (query.length > 0) {
-    countries = countries.filter((country) =>
-      country.name.toLowerCase().includes(query.toLowerCase())
+    countries = countries.filter(
+      (country) =>
+        country.name.toLowerCase().includes(query.toLowerCase()) ||
+        country.region.toLowerCase().includes(query.toLowerCase())
     );
     if (countries.length < 1)
       return (
         <div className="no-country">
-          <p>No search country</p>
+          <p>No such country or region</p>
         </div>
       );
   }
