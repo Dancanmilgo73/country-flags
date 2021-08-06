5bbd8162591b40f7bdc8704c236d2f62622ad9bd
Dan Ngetich <dancanmilgo73@gmail.com>
2021-08-06 17:05:37 +0300

    fixed input width and no country found message

--- src/Home/Countries.js
+++ src/Home/Countries.js
@@ -4,8 +4,14 @@ import Country from "./Country";
 export default function Countries({ countries, query, region, theme }) {
   if (query.length > 0) {
     countries = countries.filter((country) =>
-      country.name.toLowerCase().includes(query)
+      country.name.toLowerCase().includes(query.toLowerCase())
     );
+    if (countries.length < 1)
+      return (
+        <div className="no-country">
+          <p>No search country</p>
+        </div>
+      );
   }
   if (region.length > 0) {
     countries = countries.filter((country) =>
